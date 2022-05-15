7003e587bd2c9bf4f7e42980a39bc0658644bb22
eric2788 <tsukiko852@gmail.com>
2022-05-16 01:01:25 +0800

    added shop per permission

--- src/main/java/org/dragonitemc/dragonshop/command/DShopCommand.java
+++ src/main/java/org/dragonitemc/dragonshop/command/DShopCommand.java
@@ -1,20 +1,8 @@
 package org.dragonitemc.dragonshop.command;
 
-import com.ericlam.mc.eld.annotations.CommandArg;
 import com.ericlam.mc.eld.annotations.Commander;
-import com.ericlam.mc.eld.annotations.InjectPool;
 import com.ericlam.mc.eld.components.CommandNode;
-import com.ericlam.mc.eld.configurations.GroupConfig;
-import com.ericlam.mc.eldgui.InventoryService;
-import com.ericlam.mc.eldgui.UIDispatcher;
-import com.ericlam.mc.eldgui.UINotFoundException;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.dragonitemc.dragonshop.api.ShopService;
-import org.dragonitemc.dragonshop.config.Shop;
-
-import javax.inject.Inject;
-import java.util.Optional;
 
 @Commander(
         name = "dshop",
--- src/main/java/org/dragonitemc/dragonshop/command/DShopOpenCommand.java
+++ src/main/java/org/dragonitemc/dragonshop/command/DShopOpenCommand.java
@@ -7,12 +7,12 @@ import com.ericlam.mc.eldgui.UINotFoundException;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.dragonitemc.dragonshop.api.ShopService;
+import org.dragonitemc.dragonshop.config.DragonShopMessage;
 
 import javax.inject.Inject;
 
 @Commander(
         name = "open",
-        permission = "dragonshop.open",
         description = "打開商店",
         playerOnly = true
 )
@@ -24,10 +24,17 @@ public class DShopOpenCommand implements CommandNode {
     @Inject
     private ShopService shopService;
 
+    @Inject
+    private DragonShopMessage message;
+
 
     @Override
     public void execute(CommandSender sender) {
         try {
+            if (!sender.hasPermission("dragonshop.open."+shopName)){
+                sender.sendMessage(message.getLang().get("no-permission"));
+                return;
+            }
             var player = (Player) sender;
             shopService.openShop(player, shopName);
         } catch (UINotFoundException e) { // 找不到該界面時
