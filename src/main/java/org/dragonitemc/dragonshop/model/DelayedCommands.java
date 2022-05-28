292d12e9973144f524cc3af1b7640853943b1a9a
eric2788 <tsukiko852@gmail.com>
2022-05-28 22:24:18 +0800

    add delay property for player-command

---
+++ src/main/java/org/dragonitemc/dragonshop/model/DelayedCommands.java
@@ -0,0 +1,11 @@
+package org.dragonitemc.dragonshop.model;
+
+import java.util.List;
+
+public class DelayedCommands {
+
+    public long delay;
+
+    public List<String> commands;
+
+}
--- src/main/java/org/dragonitemc/dragonshop/tasks/rewards/PlayerCommandReward.java
+++ src/main/java/org/dragonitemc/dragonshop/tasks/rewards/PlayerCommandReward.java
@@ -1,20 +1,34 @@
 package org.dragonitemc.dragonshop.tasks.rewards;
 
 import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.dragonitemc.dragonshop.DragonShop;
 import org.dragonitemc.dragonshop.api.RewardTask;
+import org.dragonitemc.dragonshop.model.DelayedCommands;
 
 import java.util.List;
 
-public class PlayerCommandReward extends RewardTask<List<String>> {
+public class PlayerCommandReward extends RewardTask<DelayedCommands> {
 
     public PlayerCommandReward() {
         super("player-command");
     }
 
     @Override
-    public void giveReward(List<String> content, Player player) {
-        for (String line : content) {
+    public void giveReward(DelayedCommands content, Player player) {
+        if (content.delay <= 0) {
+            runCommands(player, content.commands);
+        } else {
+            Bukkit.getScheduler()
+                    .runTaskLater(DragonShop.getProvidingPlugin(DragonShop.class),
+                            () -> runCommands(player, content.commands)
+                            , content.delay);
+        }
+    }
+
+    private void runCommands(Player player, List<String> commands) {
+        for (String line : commands) {
             line = PlaceholderAPI.setPlaceholders(player, line);
             player.chat("/" + line);
         }
